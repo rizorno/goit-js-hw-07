4aad8c3013d6fd27c151d17d87123c5b192903ef
rizorno <114782234+rizorno@users.noreply.github.com>
2022-12-21 01:48:04 +0100

    updates

---
+++ js/01-gallery.js
@@ -0,0 +1,67 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+const listGallery = document.querySelector(".gallery");
+
+const list = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<div class="gallery__item">
+    <a class="gallery__link"  href="${original}">
+    <img class="gallery__image" src="${preview}", data-source="${original}", alt="${description}"/>
+    </a>
+    </div>`
+  )
+  .join("");
+
+// listGallery.innerHTML = list;
+listGallery.insertAdjacentHTML("beforeend", list);
+
+listGallery.addEventListener("click", onOpenModal);
+
+function onOpenModal(event) {
+  event.preventDefault();
+
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+
+  const srcBigImg = event.target.dataset.source;
+
+  const instance = basicLightbox.create(
+    `
+       <img src="${srcBigImg}" width="800" height="600" />
+     `
+  );
+
+  // ---------- Варіант з кнопкою "Close" ----------
+
+  //   const instance = basicLightbox.create(
+  //     `
+  //        <div class="modal">
+  //        <img class="modal-img" src="${srcBigImg}" width="70%" height="50%" />
+  //        <button type="button" class="modal-button"><b>X</b></button>
+  //        </div>
+  //      `,
+  //     {
+  //       onShow: (instance) => {
+  //         instance.element().querySelector(".modal").onclick = instance.close;
+  //       },
+  //     }
+  //   );
+
+  instance.show();
+
+  if (instance.visible()) {
+    window.addEventListener("keydown", onPressEsc);
+  }
+
+  function onPressEsc(event) {
+    if (event.code === "Escape") {
+      instance.close();
+      window.removeEventListener("keydown", onPressEsc);
+    }
+  }
+}
+
+console.log(galleryItems);
---
+++ js/02-lightbox.js
@@ -0,0 +1,33 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+const listGallery = document.querySelector(".gallery");
+
+const list = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li>
+    <a class="gallery__link"  href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}"/>
+    </a>
+    </li>
+    `
+  )
+  .join("");
+
+// listGallery.innerHTML = list;
+listGallery.insertAdjacentHTML("beforeend", list);
+
+listGallery.addEventListener("click", onOpenModal);
+
+function onOpenModal(event) {
+  event.preventDefault();
+
+  const lightbox = new SimpleLightbox(".gallery a", {
+    captions: true,
+    captionsData: "alt",
+    captionDelay: 250,
+  });
+}
+
+console.log(galleryItems);
